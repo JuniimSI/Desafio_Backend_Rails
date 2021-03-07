b68b2bbe95e441e0079ebb0f55a418047b5545fc
JuniimSI <jrfernandesti@gmail.com>
2021-03-07 00:25:52 -0300

    refactor model functions of candidato and fixed bug of alert view

--- app/models/candidato.rb
+++ app/models/candidato.rb
@@ -1,3 +1,21 @@
 class Candidato < ApplicationRecord
     has_many :despesas
+
+    def despesas_maiores
+        self.despesas.where.not(datEmissao: nil).order(vlrLiquido: :desc).limit(7)
+    end
+
+    def despesa_max
+        self.despesas.maximum(:vlrLiquido)
+    end
+
+    def despesa_somatorio
+        self.despesas.sum(:vlrLiquido)
+    end
+
+    def get_despesas
+        self.despesas.where.not(datEmissao: nil).order(vlrLiquido: :desc)
+    end
+    
+    
 end
